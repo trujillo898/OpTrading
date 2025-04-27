b504320cbe3a1a1de18dbb9870fc198662e770f2
trujillo898 <thomastrujillo898@gmail.com>
2025-04-27 17:06:45 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('trading-pro-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
